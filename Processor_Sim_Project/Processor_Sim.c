6be0f5a2b02fe6c69d3b22bcef3b7994e5659bae
Finn <finnwilkinson5@gmail.com>
2021-02-15 17:15:55 +0000

    moved files

--- Processor_Sim_Project/Processor_Sim.c
+++ Processor_Sim_Project/Processor_Sim.c
@@ -11,16 +11,19 @@ int MEM[1024];
 int INSTR[512];
 
 enum instruction{ADD, ADDI, SUB, SUBI, MUL, MULI, DIV, DIVI, LD, LDC, STR, STRC, CMP, JMP, BR, BEQ, BLT, HALT};
+enum register{r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, 
+              r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31};
 
 int main(int argc, char const *argv[])
 {
-    while(finished == 0){
+    printf("%d\n", r12);
+    /*while(finished == 0){
         fetch();
         decode():
         execute();
         instructions++;
         cycles += 3;
-    }
+    }*/
     return 0;
 }
 

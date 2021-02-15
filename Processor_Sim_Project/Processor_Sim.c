23a82dd65a71b502006dcdc1983abc53ff419f95
Finn <finnwilkinson5@gmail.com>
2021-02-15 22:44:42 +0000

    added struct for instructions

--- Processor_Sim_Project/Processor_Sim.c
+++ Processor_Sim_Project/Processor_Sim.c
@@ -6,9 +6,18 @@ int cycles = 0;
 int instructions = 0;
 int PC = 0;
 
+//structure holding an instruction and its opcodes for easier storage, passing, etc.
+struct instruction
+{
+    int opCode;
+    int operand1;
+    int operand2;
+    int operand3;
+};
+
 int RF[32];         //r0 is zero register, val is always 0
 int MEM[1024];
-int INSTR[512];
+struct instruction INSTR[512];
 
 enum instructions {ADD, ADDI, SUB, SUBI, MUL, MULI, DIV, DIVI, LD, LDC, STR, STRC, CMP, JMP, BR, BEQ, BLT, HALT};
 enum registers {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, 
@@ -20,12 +29,18 @@ void initialize(){
     {
         RF[i] = 0;
         MEM[i] = 0;
-        INSTR[i] = 0;
+        INSTR[i].opCode = 0;
+        INSTR[i].operand1 = 0;
+        INSTR[i].operand2 = 0;
+        INSTR[i].operand3 = 0;
     }
     for (int i = 32; i < 512; i++)
     {
         MEM[i] = 0;
-        INSTR[i] = 0;
+        INSTR[i].opCode = 0;
+        INSTR[i].operand1 = 0;
+        INSTR[i].operand2 = 0;
+        INSTR[i].operand3 = 0;
     }
     for (int i = 512; i < 1024; i++)
     {
@@ -42,7 +57,7 @@ void loadProgram(char *filename){
 //print current system stats (finsihed, cycles, instructions, PC, RF, CurrentInstruction, PrevInstruction, NextInstruction, etc.)
 void printSysInfo(){
     //for testing vector additon script
-    for (size_t i = 0; i < count; i++)
+    for (int i = 0; i < 10; i++)
     {
         printf("a[%d] = %d \n", i, MEM[i]);
         printf("b[%d] = %d \n", i, MEM[10 + i]);

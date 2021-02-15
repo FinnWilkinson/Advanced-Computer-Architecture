76145f5e53c35fcfec92d2141c13086e59c79fbe
Finn <finnwilkinson5@gmail.com>
2021-02-15 18:17:04 +0000

    created vector addition program. implemented some Sim functions.

--- Processor_Sim_Project/Processor_Sim.c
+++ Processor_Sim_Project/Processor_Sim.c
@@ -6,26 +6,13 @@ int cycles = 0;
 int instructions = 0;
 int PC = 0;
 
-int RF[32];
+int RF[32];         //r0 is zero register, val is always 0
 int MEM[1024];
 int INSTR[512];
 
-enum instruction{ADD, ADDI, SUB, SUBI, MUL, MULI, DIV, DIVI, LD, LDC, STR, STRC, CMP, JMP, BR, BEQ, BLT, HALT};
-enum register{r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, 
+enum instructions {ADD, ADDI, SUB, SUBI, MUL, MULI, DIV, DIVI, LD, LDC, STR, STRC, CMP, JMP, BR, BEQ, BLT, HALT};
+enum registers {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, 
               r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31};
-
-int main(int argc, char const *argv[])
-{
-    printf("%d\n", r12);
-    /*while(finished == 0){
-        fetch();
-        decode():
-        execute();
-        instructions++;
-        cycles += 3;
-    }*/
-    return 0;
-}
 
 //initialise data and registers to 0.
 void initialize(){
@@ -46,8 +33,52 @@ void initialize(){
     }   
 }
 
-void printSysInfo(){
-    //print current system stats (finsihed, cycles, instructions, PC, RF, CurrentInstruction, PrevInstruction, NextInstruction)
+//load specified assembly program
+void loadProgram(char *filename){
+    //igrnore any '//' in the input program
+    printf("%s\n", filename);
 }
 
+//print current system stats (finsihed, cycles, instructions, PC, RF, CurrentInstruction, PrevInstruction, NextInstruction, etc.)
+void printSysInfo(){
+    //for testing vector additon script
+    for (size_t i = 0; i < count; i++)
+    {
+        printf("a[%d] = %d \n", i, MEM[i]);
+        printf("b[%d] = %d \n", i, MEM[10 + i]);
+        printf("c[%d] = %d \n", i, MEM[20 + i]);
+    }
+    
+}
 
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+void fetch(){
+
+}
+
+void decode(){
+
+}
+
+void execute(){
+
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+int main(int argc, char const *argv[])
+{
+    initialize();
+    loadProgram("VectorAddition.txt");
+    /*while(finished == 0){
+        fetch();
+        decode():
+        execute();
+        instructions++;
+        cycles += 3;
+    }*/
+    return 0;
+}
